3caf369a0c9ea8287f2a8243c343b623391b07b6
Joshua <jross3740@gmail.com>
2017-01-01 17:35:12 -0500

    Update PrepareItemCraftEvent.php

--- src/GenisysFTW/events/PrepareItemCraftEvent.php
+++ src/GenisysFTW/events/PrepareItemCraftEvent.php
@@ -1,1 +1,39 @@
+<?php
 
+namespace GenisysFTW\events;
+
+use pocketmine\event\Cancellable;
+use pocketmine\inventory\Inventory;
+use pocketmine\item\Item;
+use pocketmine\Player;
+use pocketmine\event\inventory\InventoryEvent;
+use pocketmine\inventory\CraftingInventory;
+
+// In Dev!
+
+class PrepareItemCraftEvent extends InventoryEvent implements Cancellable{
+  
+  public static $handlerList = null;
+  private $repair;
+  private $what;
+  
+  public function __construct(CraftingInventory $what, InventoryEvent $view, (bool) $repair){
+    $this->what = $what;
+    $this->view = $view->getViewers();
+    $this->repair = $repair;
+  }
+  public function getRecipe(){
+    return $this->what->getRecipe();
+  }
+  public function getInventory(){
+    return $this->what;
+  }
+  
+  /**
+     * @return Player
+   */
+  public function getWho(){
+    return $this->view;
+  }
+  
+}
